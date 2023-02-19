818eda7f10c855748b267c71c733bfa02634a323
mfmstr <g.kharebava01@gmail.com>
2023-02-19 16:47:58 +0400

    initial commit

---
+++ Desktop/Spring Projects/springbootrestapitablesrelations/src/main/java/com/gk/tablesrelations/SpringbootrestapitablesrelationsApplication.java
@@ -0,0 +1,13 @@
+package com.gk.tablesrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestapitablesrelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestapitablesrelationsApplication.class, args);
+	}
+
+}
---
+++ Desktop/Spring Projects/springbootrestapitablesrelations/src/test/java/com/gk/tablesrelations/SpringbootrestapitablesrelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.tablesrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestapitablesrelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
